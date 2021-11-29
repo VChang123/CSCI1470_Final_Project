733768b686d9f7aa1d5b6c9494976946e4578862
dangdiv20 <divyam_dang@brown.edu>
2021-11-29 17:44:31 -0500

    Preprocess

--- code/preprocess.py
+++ code/preprocess.py
@@ -8,86 +8,86 @@ import csv
 import imageio
 from extract import Extractor
 
-def load_categories():
+# def load_categories():
     
-    with open('data/categories.txt', 'r') as desc:
+#     with open('data/categories.txt', 'r') as desc:
         
-        lines = desc.readline()
-        categories = lines.split(" ")
+#         lines = desc.readline()
+#         categories = lines.split(" ")
         
-        return categories
+#         return categories
 
-def load_data(resize):
-    # Find all the Image Files
-    image_files = open("./data/groundtruth_train.tsv")
-    read_tsv = csv.reader(image_files, delimiter="\t")
+# def load_data(resize):
+#     # Find all the Image Files
+#     image_files = open("./data/groundtruth_train.tsv")
+#     read_tsv = csv.reader(image_files, delimiter="\t")
 
-    symbols = load_categories()
-    # print(symbols)
+#     symbols = load_categories()
+#     # print(symbols)
 
-    images = []
+#     images = []
 
-    a = 0
+#     a = 0
     
-    for row in read_tsv:
-        #image path
-        path = row[0]
-        path = path + ".png"
-        image_path = os.path.join("./data/train/", path)
+#     for row in read_tsv:
+#         #image path
+#         path = row[0]
+#         path = path + ".png"
+#         image_path = os.path.join("./data/train/", path)
 
-        # Open the Image
-        img = Image.open(image_path)
-        # print(img)
+#         # Open the Image
+#         img = Image.open(image_path)
+#         # print(img)
         
-        # Rescale the Image to the Appropriate Size
-        img = img.resize(resize)
+#         # Rescale the Image to the Appropriate Size
+#         img = img.resize(resize)
 
-        # Convert from ints to floats
-        img = np.array(img)
+#         # Convert from ints to floats
+#         img = np.array(img)
 
-        images.append(img)
+#         images.append(img)
 
-        # label = row[1]
+#         # label = row[1]
 
-        # label = label.split()
+#         # label = label.split()
 
-        # final_label = []
+#         # final_label = []
 
-        # for i in label:
-        #     if i in symbols:
-        #         final_label.append(i)
-        #     else:
-        #         j = re.sub("\_*", " " , i)
-        #         j = re.sub("\{*", " ", j)
-        #         j = re.sub("\}*", " ", j)
-        #         j = re.sub("\{*", " ", j)
+#         # for i in label:
+#         #     if i in symbols:
+#         #         final_label.append(i)
+#         #     else:
+#         #         j = re.sub("\_*", " " , i)
+#         #         j = re.sub("\{*", " ", j)
+#         #         j = re.sub("\}*", " ", j)
+#         #         j = re.sub("\{*", " ", j)
                     
-        # print(final_label)
-        # if a == 1:
-        #     return
+#         # print(final_label)
+#         # if a == 1:
+#         #     return
 
-        # a+=1
-
-
-
-
-        # # arr1 = label.split()
-        # # labels = []
-        # # for i in arr1:
-        # #     if i in symbols:
-        # # return
+#         # a+=1
 
 
 
-    images = np.asarray(images)
 
-    # Convert from ints to floats
-    images = images.astype('float32')
+#         # # arr1 = label.split()
+#         # # labels = []
+#         # # for i in arr1:
+#         # #     if i in symbols:
+#         # # return
 
-    # Scale from [0,255] to [-1,1]
-    images = images/255
 
-    return images
+
+#     images = np.asarray(images)
+
+#     # Convert from ints to floats
+#     images = images.astype('float32')
+
+#     # Scale from [0,255] to [-1,1]
+#     images = images/255
+
+#     return images
 
 def get_data():
     extractor = Extractor(32, "2012")
